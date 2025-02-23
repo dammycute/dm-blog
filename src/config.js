abbf3c871e3298e360b8ade2690fc9a232453b04
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2025-02-23 22:26:04 +0100

    Remove: Delete the url error

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const API_URL = {
-    BaseUrl: "http://127.0.0.1:8000"
+    BaseUrl: "https://lesser-hawk-htcode-9cbcc466.koyeb.app"
 }
 
 export default API_URL;
-
-// https://lesser-hawk-htcode-9cbcc466.koyeb.app
